4459651821a1735fe8392b339bc9b77cf12b34e7
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-04-17 12:29:44 -0500

    title component and colors constant

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,10 @@
 import { View, Text, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen() {
     return (
             <View style={styles.screen}>
-                <Text>
-                    Opponent's Guess
-                </Text>
-                {/* GUESS */}
+                <Title>Opponent's Guess</Title>
                 <View>
                     <Text>Higher or Lower?</Text>
                     <Button></Button>
@@ -23,6 +21,15 @@ export default GameScreen;
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
+        padding: 24
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#ddb52f',
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: '#ddb52f',
         padding: 12
     }
 
