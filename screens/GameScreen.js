dfa4e83c8d7ed00eab73b55c95813ea054b3a624
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-04-17 20:25:17 -0500

    number component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { useState } from 'react';
-import Title from '../components/Title';
+import Title from '../components/ui/Title';
 import Colors from '../constants/colors';
+import NumberContainer from '../components/game/NumberContainer';
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max-min)) + min;
@@ -21,6 +22,7 @@ function GameScreen({userNumber}) {
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
+            <NumberContainer>{currentGuess}</NumberContainer>
             <View>
                 <Text>Higher or Lower?</Text>
             </View>
