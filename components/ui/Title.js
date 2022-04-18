c16231507a300ff77f13617b9117ab9aa1cf20b3
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-04-17 20:25:06 -0500

    separated out the number container component

---
+++ components/ui/Title.js
@@ -0,0 +1,25 @@
+import { Text, StyleSheet } from 'react-native';
+import Color from '../../constants/colors';
+
+//uses descructuring to call the children to populate the actual text. the children is provided
+//by the opening and closing tag of the component which <Title /> is used
+
+function Title({children}) {
+    return (
+        <Text style={styles.title}>{children}</Text>
+    )
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: Color.accent500,
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: Color.accent500,
+        padding: 40,
+    }
+})
